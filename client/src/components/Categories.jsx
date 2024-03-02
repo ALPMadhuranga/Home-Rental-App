0afb6e8a3e36aa02a99df50fda838ee394d5b17a
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-03-02 23:41:36 +0530

    initial commit

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import { categories } from "../data";
+import "../styles/Categories.scss"
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <h1>Discover the Primary Categories</h1>
+      <p>
+      Discover our extensive selection of holiday accommodations designed to suit every kind of traveler. Immerse yourself in the essence of the local community, relish the familiarity of home comforts, and forge lasting memories in your ideal getaway location.
+      </p>
+
+      <div className="categories_list">
+        {categories?.slice(1, 7).map((category, index) => (
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
+              <img src={category.img} alt={category.label} />
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
---
+++ client/src/components/Slide.jsx
@@ -0,0 +1,14 @@
+import "../styles/Slide.scss"
+
+const Slide = () => {
+  return (
+    <div className="slide">
+      <h1>
+       Welcome Home to Your Next Adventure! <br />
+       Discover Your Perfect Stay and Create Lasting Memories
+      </h1>
+    </div>
+  );
+};
+
+export default Slide;
