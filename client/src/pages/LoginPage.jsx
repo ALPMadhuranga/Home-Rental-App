6386ab73a092a79d0c20393b5f1c529368416827
ALPMadhuranga <lahiruprasad520@gmail.com>
2024-03-03 01:55:43 +0530

    change routes

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch ("https://home-rental-app-ocw9.vercel.app//auth/login", {
+      const response = await fetch ("https://home-rental-app-ocw9.vercel.app/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
